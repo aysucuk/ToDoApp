c0aa5fa603acdc8317131fe6d96fa86b4b779660
aysucuk <aysusadixova@gmail.com>
2025-03-05 23:33:05 +0400

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Aysu Sadıxova on 05.03.25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Aysu Sadıxova on 05.03.25.
+//
+
+import Testing
+@testable import ToDoApp
+
+struct ToDoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
